0a24a6c036a66ded84a2f2a72202ffb4e6702501
jacobnathanson <jacobnath93@gmail.com>
2022-01-18 15:36:58 -0500

    Navigation Part 1

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,27 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
 
-function Directory(props) {
+class Directory extends Component {
 
-    const renderDirectoryItem = ({ item }) => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES
+        };
+    }
+
+    // sets a method on the class itself
+    static navigationOptions = {
+        title: 'Directory'
+    }
+
+    // directory is a 'screen' so it has props.navigation from main
+    render() {
+        const { navigate } = this.props.navigation;
+        const renderDirectoryItem = ({ item }) => {
+            return (
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
+                    leftAvatar={{ source: require('./images/react-lake.jpg') }}
+                />
+            );
+        };
+
         return (
-            <ListItem
-                title={item.name}
-                subtitle={item.description}
-                onPress={() => props.onPress(item.id)}
-                leftAvatar={{ source: require('./images/react-lake.jpg') }}
+            <FlatList
+                data={this.state.campsites}
+                renderItem={renderDirectoryItem}
+                keyExtractor={item => item.id.toString()}
             />
         );
-    };
-
-    return (
-        <FlatList
-            data={props.campsites}
-            renderItem={renderDirectoryItem}
-            keyExtractor={item => item.id.toString()}
-        />
-    );
+    }
 }
 
 export default Directory;
